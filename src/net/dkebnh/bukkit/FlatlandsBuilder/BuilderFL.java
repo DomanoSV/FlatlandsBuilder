5a1951123296c403655d03e65c9af809212647d8
DomanoSV <domano.sv@gmail.com>
2012-03-31 15:20:04 +1000

    Started Project Enjoy!!!

---
+++ src/net/dkebnh/bukkit/FlatlandsBuilder/BuilderFL.java
@@ -0,0 +1,24 @@
+package net.dkebnh.bukkit.FlatlandsBuilder;
+
+import java.util.logging.Logger;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BuilderFL extends JavaPlugin {
+	
+	private Logger log = Logger.getLogger("Minecraft");
+	
+	public void onEnable(){
+		this.logMessage("Enabled."); 		
+	}
+	
+	public void onDisable(){
+		this.logMessage("Disabled."); 
+	}
+	
+	public void logMessage(String msg){
+		PluginDescriptionFile pdFile = this.getDescription();
+		this.log.info(pdFile.getName() + " " + pdFile.getVersion() + " - " + msg);
+	}
+	
+}
