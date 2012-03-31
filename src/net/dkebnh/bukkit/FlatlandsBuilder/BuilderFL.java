11363fec1a8428086ecd1389c427c51f7b7c475a
DomanoSV <domano.sv@gmail.com>
2012-03-31 23:23:49 +1000

    World Generator Working, Now to populating each chunk

--- src/net/dkebnh/bukkit/FlatlandsBuilder/BuilderFL.java
+++ src/net/dkebnh/bukkit/FlatlandsBuilder/BuilderFL.java
@@ -1,6 +1,8 @@
 package net.dkebnh.bukkit.FlatlandsBuilder;
 
 import java.util.logging.Logger;
+
+import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -19,6 +21,11 @@ public class BuilderFL extends JavaPlugin {
 	public void logMessage(String msg){
 		PluginDescriptionFile pdFile = this.getDescription();
 		this.log.info(pdFile.getName() + " " + pdFile.getVersion() + " - " + msg);
+	}
+	
+	public ChunkGenerator getDefaultWorldGenerator(String worldName, String uid){
+		return new FLBGenerator(this);
+		
 	}
 	
 }
---
+++ src/net/dkebnh/bukkit/FlatlandsBuilder/FLBGenerator.java
@@ -0,0 +1,51 @@
+package net.dkebnh.bukkit.FlatlandsBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World; 
+import org.bukkit.generator.BlockPopulator;
+import org.bukkit.generator.ChunkGenerator;
+
+public class FLBGenerator extends ChunkGenerator{
+	
+	private BuilderFL plugin;
+	
+	public FLBGenerator(BuilderFL instance){
+		this.plugin = instance;
+	}
+	
+	public List<BlockPopulator> getDefaultPopulators(World world){
+		return new ArrayList<BlockPopulator>();
+	}
+	
+	public Location getFixedSpawnLocation(World world, Random random){
+		return new Location(world, 0, 17, 0);
+	}
+	
+	private int coordsToInt(int x, int y, int z){
+			return (x * 16 + z) * 256 + y;
+	}
+	
+	public byte[] generate(World world, Random rand, int chunkx, int chunkz){
+		byte[] blocks = new byte[65536];
+		int x, y, z;
+		
+		for (x = 0; x < 16; ++x){
+			for (z = 0; z < 16; ++z){
+				blocks[this.coordsToInt(x, 0, z)] = (byte) Material.BEDROCK.getId();
+				
+				for (y = 1; y < 15; ++y){
+					blocks[this.coordsToInt(x, y, z)] = (byte) Material.STONE.getId();
+				}
+
+				blocks[this.coordsToInt(x, 15, z)] = (byte) Material.WOOL.getId();
+			}
+			}
+		
+			return blocks;
+	}
+}
