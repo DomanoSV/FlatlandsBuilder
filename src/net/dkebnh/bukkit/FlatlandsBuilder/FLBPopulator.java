a1e315bb42715d4885265221fdc4ada8a71951a7
DomanoSV <domano.sv@gmail.com>
2012-04-01 10:36:56 +1000

    Added the ability to use colored wool and updated README

--- src/net/dkebnh/bukkit/FlatlandsBuilder/FLBPopulator.java
+++ src/net/dkebnh/bukkit/FlatlandsBuilder/FLBPopulator.java
@@ -5,16 +5,30 @@ import java.util.Random;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.generator.BlockPopulator;
 
 public class FLBPopulator extends BlockPopulator {
 
 	public void populate(World world, Random random, Chunk chunk) {
-		int blockx, blockz;
+		int blockx, blockz, blockx2, blockz2;
+		Block block;
+		
+		for (blockx2 = 0; blockx2 < 16; ++blockx2){
+			for (blockz2 = 0; blockz2 < 16; ++blockz2){
+				block = chunk.getBlock(blockx2, 15, blockz2);
+						
+				block.setType(Material.WOOL);
+				block.setData((byte) 0x7);
+				}
+		}
 		
 		for (blockx = 1; blockx < 15; ++blockx){
 			for (blockz = 1; blockz < 15; ++blockz){
-				chunk.getBlock(blockx, 15, blockz).setType(Material.COBBLESTONE);
+				block = chunk.getBlock(blockx, 15, blockz);
+						
+				block.setType(Material.WOOL);
+				block.setData((byte) 0xf);
 				}
 		}
 	}
