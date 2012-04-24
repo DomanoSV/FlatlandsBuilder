e5938a99509d23ef5c8a92307fee1ca670ecf6f9
DomanoSV <domano.sv@gmail.com>
2012-04-24 20:00:29 +1000

    Added Alternating Borders as Planned, Start optimisation now.

--- src/net/dkebnh/bukkit/FlatlandsBuilder/FLBGenerator.java
+++ src/net/dkebnh/bukkit/FlatlandsBuilder/FLBGenerator.java
@@ -18,21 +18,25 @@ public class FLBGenerator extends ChunkGenerator{
 	private int height;
 	private Material BlockA;
 	private Material BlockB;
+	private Material BlockC;
 	private byte BlockADV;
 	private byte BlockBDV;
+	private byte BlockCDV;
 	
 	public void setDefaults(String msg){
         height = 64;
-        genMode = "grid";
+        genMode = "grid2";
         BlockA = Material.WOOL;
 		BlockB = Material.WOOL;
+		BlockC = Material.WOOL;
 		BlockADV = (byte) 0x7;
 		BlockBDV = (byte) 0xf;
+		BlockCDV = (byte) 0x8;
 		this.log.warning(msg);
 	}
 	
 	public FLBGenerator(){
-		this("64,wool:15,wool:7");
+		this("64,wool:15,wool:7,wool:8");
 	}
 	
 	public FLBGenerator(String id){
@@ -41,7 +45,118 @@ public class FLBGenerator extends ChunkGenerator{
 				if (id.length() > 0){
 					String tokens[] = id.split("[,]");
 					
-					if (tokens.length == 3){
+					if (tokens.length == 4){
+						height = Integer.parseInt(tokens[0]);
+							if (height <= 0 || height >= 255){ // Will change max height later on, once sure 256 is the maximum value and not lower.
+								log.warning("[FlatlandsBuilder] Invalid height '" + tokens[0] + "'. Using 64 instead.");
+								height = 64;
+							} 
+						genMode = "grid2";
+						
+				        String blockAmaterialTokens[] = tokens[2].split("[:]", 2);
+				        String blockBmaterialTokens[] = tokens[1].split("[:]", 2);
+				        String blockCmaterialTokens[] = tokens[3].split("[:]", 2);
+				        
+				        if (blockAmaterialTokens.length == 2){
+				            try{
+				            	BlockADV = Byte.parseByte(blockAmaterialTokens[1]);
+				            }catch (Exception e){
+				            			log.warning("[FlatlandsBuilder] Invalid Border Block Data Value '" + blockAmaterialTokens[1] + "'. Defaulting to 0.");
+				            			BlockADV = (byte) 0x0;
+				            }
+				        }
+				        
+				        Material blockAmat = Material.matchMaterial(blockAmaterialTokens[0]);
+				        
+				        if (blockAmat == null){
+				            try{
+				            	blockAmat = Material.getMaterial(Integer.parseInt(blockAmaterialTokens[0]));
+				            }catch (Exception e){
+
+				            }
+				            
+				            if (blockAmat == null){
+				            	log.warning("[FlatlandsBuilder] Invalid Border Block ID '" + blockAmaterialTokens[0] + "'. Defaulting to WHITE_WOOL.");
+				            	blockAmat = Material.WOOL;
+				            }
+				        }
+				        
+				        if (!blockAmat.isBlock()){
+				            log.warning("[FlatlandsBuilder] Error, Border Block'" + blockAmaterialTokens[0] + "' is not a block. Defaulting to WHITE_WOOL.");
+				            blockAmat = Material.WOOL;
+				        }
+				        
+				        if (blockBmaterialTokens.length == 2){
+				            try{
+				            	BlockBDV = Byte.parseByte(blockBmaterialTokens[1]);
+				            }catch (Exception e){
+				                log.warning("[FlatlandsBuilder] Invalid Fill Block Data Value '" + blockBmaterialTokens[1] + "'. Defaulting to 0.");
+				                BlockBDV = (byte) 0x0;
+				            }
+				        }
+				    	
+				        Material blockBmat = Material.matchMaterial(blockBmaterialTokens[0]);
+				        
+				        if (blockBmat == null)
+				        {
+				            try
+				            {
+				            	blockBmat = Material.getMaterial(Integer.parseInt(blockBmaterialTokens[0]));
+				            } catch (Exception e){
+
+				            }
+
+				            if (blockBmat == null)
+				            {
+				                log.warning("[FlatlandsBuilder] Invalid Fill Block ID '" + blockBmaterialTokens[0] + "'. Defaulting fill to WHITE_WOOL.");
+				                blockBmat = Material.WOOL;
+				            }
+				        }
+				        
+				        if (!blockBmat.isBlock())
+				        {
+				            log.warning("[FlatlandsBuilder] Error, Fill Block '" + blockBmaterialTokens[0] + "' is not a block. Defaulting fill to WHITE_WOOL.");
+				            blockBmat = Material.WOOL;
+				        }
+				        
+				        if (blockCmaterialTokens.length == 2){
+				            try{
+				            	BlockCDV = Byte.parseByte(blockCmaterialTokens[1]);
+				            }catch (Exception e){
+				                log.warning("[FlatlandsBuilder] Invalid Fill Block Data Value '" + blockCmaterialTokens[1] + "'. Defaulting to 0.");
+				                BlockCDV = (byte) 0x0;
+				            }
+				        }
+				    	
+				        Material blockCmat = Material.matchMaterial(blockCmaterialTokens[0]);
+				        
+				        if (blockCmat == null)
+				        {
+				            try
+				            {
+				            	blockCmat = Material.getMaterial(Integer.parseInt(blockCmaterialTokens[0]));
+				            } catch (Exception e){
+
+				            }
+
+				            if (blockCmat == null)
+				            {
+				                log.warning("[FlatlandsBuilder] Invalid Fill Block ID '" + blockCmaterialTokens[0] + "'. Defaulting fill to WHITE_WOOL.");
+				                blockCmat = Material.WOOL;
+				            }
+				        }
+				        
+				        if (!blockCmat.isBlock())
+				        {
+				            log.warning("[FlatlandsBuilder] Error, Fill Block '" + blockCmaterialTokens[0] + "' is not a block. Defaulting fill to WHITE_WOOL.");
+				            blockCmat = Material.WOOL;
+				        }
+
+				        BlockA = blockAmat;
+				        BlockB = blockBmat;
+				        BlockC = blockCmat;
+				        
+					}else if (tokens.length == 3){
 						height = Integer.parseInt(tokens[0]);
 							if (height <= 0 || height >= 128){ // Will change max height later on, once sure 256 is the maximum value and not lower.
 								log.warning("[FlatlandsBuilder] Invalid height '" + tokens[0] + "'. Using 64 instead.");
@@ -160,30 +275,32 @@ public class FLBGenerator extends ChunkGenerator{
 					        BlockA = blockAmat;
 					        
 						}else{
-							this.setDefaults("[FlatlandsBuilder] Invalid Settings provided, using defaults '64,wool:15,wool:7'");
+							this.setDefaults("[FlatlandsBuilder] Invalid Settings provided, using defaults '64,wool:15,wool:7,wool:8'");
 						}
 					} 
 				} 
 			}catch (Exception e){
-			log.severe("[FlatlandsBuilder] Error parsing FlatlandsBuilder Settings '" + id + "'. using defaults '64,wool:15,wool:7': " + e.toString());
+			log.severe("[FlatlandsBuilder] Error parsing FlatlandsBuilder Settings '" + id + "'. using defaults '64,wool:15,wool:7,wool:8': " + e.toString());
             e.printStackTrace();
             height = 64;
-            genMode = "grid";
+            genMode = "grid2";
             BlockA = Material.WOOL;
 			BlockB = Material.WOOL;
+			BlockC = Material.WOOL;
 			BlockADV = (byte) 0x7;
 			BlockBDV = (byte) 0xf;
+			BlockCDV = (byte) 0x8;
 			}
 			
 		}else{
-			this.setDefaults("[FlatlandsBuilder] No Settings provided, using defaults '64,wool:15,wool:7'");
+			this.setDefaults("[FlatlandsBuilder] No Settings provided, using defaults '64,wool:15,wool:7,wool:8'");
 		}
 	}
 	
 	public List<BlockPopulator> getDefaultPopulators(World world){
 		ArrayList<BlockPopulator> populators = new ArrayList<BlockPopulator>();
-		
-		populators.add(new FLBPopulator(height, BlockA, BlockB, BlockADV, BlockBDV, genMode));
+
+		populators.add(new FLBPopulator(height, BlockA, BlockB, BlockC, BlockADV, BlockBDV, BlockCDV, genMode));
 		
 		return populators;
 	}
--- src/net/dkebnh/bukkit/FlatlandsBuilder/FLBPopulator.java
+++ src/net/dkebnh/bukkit/FlatlandsBuilder/FLBPopulator.java
@@ -14,32 +14,44 @@ public class FLBPopulator extends BlockPopulator {
 	private int height;
 	private Material BlockA;
 	private Material BlockB;
+	private Material BlockC;
 	private byte BlockADV;
 	private byte BlockBDV;
+	private byte BlockCDV;
+
 	
-    protected FLBPopulator(int height, Material BlockA, Material BlockB, byte BlockADV, byte BlockBDV, String genMode){
+    protected FLBPopulator(int height, Material BlockA, Material BlockB, Material BlockC, byte BlockADV, byte BlockBDV, byte BlockCDV, String genMode){
     	this.genMode = genMode;
         this.height = height;
         this.BlockA = BlockA;
         this.BlockB = BlockB;
+        this.BlockC = BlockC;
         this.BlockADV = BlockADV;
         this.BlockBDV = BlockBDV;
+        this.BlockCDV = BlockCDV;
+
     }
     
 	public void populate(World world, Random random, Chunk chunk) {
 		int blockx, blockz, blockx2, blockz2;
 		Block block;
        
-        for (blockx2 = 0; blockx2 < 16; ++blockx2){
-        	for (blockz2 = 0; blockz2 < 16; ++blockz2){
-        		block = chunk.getBlock(blockx2, height, blockz2);	
-        		block.setType(BlockA);
-        		block.setData(BlockADV);
-			}
-        }
-        
-        if (genMode == "grid"){
-            
+        if(genMode == "normal" || genMode == null){
+        	for(blockx2 = 0; blockx2 < 16; ++blockx2){
+        		for(blockz2 = 0; blockz2 < 16; ++blockz2){
+        			block = chunk.getBlock(blockx2, height, blockz2);	
+        			block.setType(BlockA);
+        			block.setData(BlockADV);
+				}
+        	}
+        }else if(genMode == "grid"){
+        	for(blockx2 = 0; blockx2 < 16; ++blockx2){
+        		for(blockz2 = 0; blockz2 < 16; ++blockz2){
+        			block = chunk.getBlock(blockx2, height, blockz2);	
+        			block.setType(BlockA);
+        			block.setData(BlockADV);
+				}
+        	}
         	for (blockx = 1; blockx < 15; ++blockx){
         		for (blockz = 1; blockz < 15; ++blockz){
         			block = chunk.getBlock(blockx, height, blockz);
@@ -48,6 +60,35 @@ public class FLBPopulator extends BlockPopulator {
         			block.setData(BlockBDV);
 				}
         	}
-        }
+        }else if(genMode == "grid2"){
+			int chunkx = chunk.getX();
+			int chunkz = chunk.getZ();
+		
+			if((chunkx + chunkz) % 2 == 0) {
+		        for(blockx2 = 0; blockx2 < 16; ++blockx2){
+		        	for(blockz2 = 0; blockz2 < 16; ++blockz2){
+		        		block = chunk.getBlock(blockx2, height, blockz2);	
+		        		block.setType(BlockA);
+		        		block.setData(BlockADV);
+					}
+		        }
+        	}else{
+		        for(blockx2 = 0; blockx2 < 16; ++blockx2){
+		        	for(blockz2 = 0; blockz2 < 16; ++blockz2){
+		        		block = chunk.getBlock(blockx2, height, blockz2);	
+		        		block.setType(BlockC);
+		        		block.setData(BlockCDV);
+					}
+		        }
+        	} 
+			
+			for(blockx = 1; blockx < 15; ++blockx){
+        		for(blockz = 1; blockz < 15; ++blockz){
+        			block = chunk.getBlock(blockx, height, blockz);
+        			block.setType(BlockB);
+        			block.setData(BlockBDV);
+				}
+        	}
+		}
     }
 }
